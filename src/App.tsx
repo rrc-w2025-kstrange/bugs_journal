aa6d6fbcb3ed158346e3f4ecc8f07ba3fb0f2a32
Kyra Strange <kstrangeguajardo@academic.rrc.ca>
2026-09-17 00:25:39 -0500

    add main tag around Bugs so the page has a proper main content area

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,13 @@ import { Header } from "./components/common/header/Header";
 import { Bugs } from "./components/bugs/Bugs";
 import { Footer } from "./components/common/footer/Footer";
 
-
-
 export function App() {
     return (
         <>
             <Header />
-            <Bugs />
+            <main>
+                <Bugs />
+            </main>
             <Footer />
         </>
     );
