146206c96c08c0544dbd8d8c6845177fb7c0401c
Kyra Strange <kstrangeguajardo@academic.rrc.ca>
2026-09-16 21:29:39 -0500

    Add footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import { Header } from "./components/common/header/Header";
 import { Bugs } from "./components/bugs/Bugs";
+import { Footer } from "./components/common/footer/Footer";
+
+
 
 export function App() {
     return (
         <>
             <Header />
             <Bugs />
+            <Footer />
         </>
     );
 }
